d34a48e760bf3600ecc11a355ea33a8c81e605bd
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-05-31 16:22:54 -0400

    aruco y dibujos de contornos. Bug de linea
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -50,10 +50,99 @@ class _MyAppState extends State<MyApp> {
     var (corners, ids, rejected) = aruco.detectMarkers(grey);
 
     if (corners.isNotEmpty) {
-      arucoDrawDetectedMarkers(rgb, corners, ids, Scalar.red);
+      //VecVecPoint2f to VecVecPoint
+      var convertCorners = convertVecVecPoint2fToVecVecPoint(corners);
+      var objectsCnt = detectObjects(rgb);
 
+      //Dibujar lineas sobre aruco
+      polylines(rgb, convertCorners, true, Scalar(0, 255, 0, 150),
+          thickness: 5);
 
+      //VecVecPoint to VecPoint (SIMPLIFIED)
+      var simplifiedCorners = convertVecVecPointToVecPoint(convertCorners);
 
+      var aruco_perimeter = arcLength(simplifiedCorners, true);
+
+      //Calcular rectangulo de aruco
+      var arucoRect = minAreaRect(simplifiedCorners);
+
+      var pixelCmRatio = aruco_perimeter / 20;
+
+      //Escribir ratio en pantalla
+      putText(
+          rgb,
+          "Pixel/cm: $pixelCmRatio",
+          Point((arucoRect.center.x - 100).toInt(),
+              (arucoRect.center.y - 100).toInt()),
+          2,
+          1,
+          Scalar.fromRgb(0, 255, 255),
+          thickness: 2);
+
+      for (var cnt in objectsCnt) {
+        var rect = minAreaRect(cnt);
+
+        var x = rect.center.x;
+        var y = rect.center.y;
+        var h = rect.boundingRect.height;
+        var w = rect.boundingRect.width;
+
+        var objectWidth = w / pixelCmRatio;
+        var objectHeight = h / pixelCmRatio;
+
+        var box = boxPoints(rect);
+        print("box celular ${box}");
+
+        Point2f previousPoint = Point2f(0, 0);
+
+        for (Point2f point2f in box) {
+          // Dibuja una línea desde el punto anterior hasta el punto actual
+          line(
+              rgb,
+              Point(previousPoint.x.toInt(), previousPoint.y.toInt()),
+              Point(point2f.x.toInt(), point2f.y.toInt()),
+              Scalar.fromRgb(255, 0, 0),
+              thickness: 2);
+
+          // Actualiza el punto anterior
+          previousPoint = point2f;
+        }
+
+        // Dibuja una línea desde el último punto hasta el primer punto para cerrar la caja
+        if (box.isNotEmpty) {
+          line(
+              rgb,
+              Point(previousPoint.x.toInt(), previousPoint.y.toInt()),
+              Point(box.first.x.toInt(), box.first.y.toInt()),
+              Scalar.fromRgb(255, 0, 0),
+              thickness: 2);
+        }
+
+        //Draw circle
+        circle(rgb, Point(x.toInt(), y.toInt()), 15, Scalar.blue,
+            thickness: -1);
+
+        //polylines(rgb, convertVecPoint2fToVecVecPoint(box), true,
+        //    Scalar.fromRgb(255, 0, 0));
+
+        putText(
+            rgb,
+            "Ancho ${double.parse(objectWidth.toStringAsFixed(2))} cm",
+            Point((x - 100).toInt(), (y - 20).toInt()),
+            FONT_HERSHEY_PLAIN,
+            2,
+            Scalar.fromRgb(100, 200, 0),
+            thickness: 2);
+
+        putText(
+            rgb,
+            "Alto ${double.parse(objectHeight.toStringAsFixed(2))} cm",
+            Point((x - 100).toInt(), (y + 15).toInt()),
+            FONT_HERSHEY_PLAIN,
+            2,
+            Scalar.fromRgb(100, 200, 0),
+            thickness: 2);
+      }
     }
 
     print("Corners ${corners}, + IDS $ids");
@@ -62,6 +151,92 @@ class _MyAppState extends State<MyApp> {
       imencode(ImageFormat.png.ext, grey),
       imencode(ImageFormat.png.ext, rgb)
     );
+  }
+
+  List<VecPoint> detectObjects(Mat frame) {
+    //Image frame to grayscale
+    var gray = Mat.create();
+    cvtColor(frame, COLOR_BGR2GRAY, dst: gray);
+
+    var mask = Mat.create();
+    adaptiveThreshold(
+        gray, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 19, 5,
+        dst: mask);
+
+    //Find contours
+    var (contours, hierarchy) =
+        findContours(mask, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+    List<VecPoint> objectContours = [];
+    for (var cnt in contours) {
+      var area = contourArea(cnt);
+
+      print("Area contours: $area");
+
+      if (area > 1000) {
+        objectContours.add(cnt);
+      }
+    }
+
+    print("Objectos detectados: ${objectContours.length}");
+
+    return objectContours;
+  }
+
+  ///Convertir VecVecPoint2f a VecVecPoint
+  VecVecPoint convertVecVecPoint2fToVecVecPoint(VecVecPoint2f vecVecPoint2f) {
+    List<List<Point>> listVecVecPoint = [];
+
+    for (var vecPoint2f in vecVecPoint2f) {
+      List<Point> listVecPoint = [];
+      for (var point2f in vecPoint2f) {
+        // Convierte cada Point2f a un Point
+        Point point = Point(point2f.x.toInt(), point2f.y.toInt());
+        listVecPoint.add(point);
+      }
+      listVecVecPoint.add(listVecPoint);
+    }
+
+    // Convierte la lista de listas de Point a VecVecPoint
+    VecVecPoint vecVecPoint = VecVecPoint.fromList(listVecVecPoint);
+
+    return vecVecPoint;
+  }
+
+  VecPoint convertVecVecPointToVecPoint(VecVecPoint vecVecPoint) {
+    List<Point> listPoint = [];
+
+    for (var vecPoint in vecVecPoint) {
+      for (var point in vecPoint) {
+        // Agrega cada Point al listPoint
+        listPoint.add(point);
+      }
+    }
+
+    // Convierte la lista de Point a VecPoint
+    VecPoint vecPoint = VecPoint.fromList(listPoint);
+
+    return vecPoint;
+  }
+
+  VecVecPoint convertVecPoint2fToVecVecPoint(VecPoint2f vecPoint2f) {
+    List<List<Point>> listVecVecPoint = [];
+
+    for (var point2f in vecPoint2f) {
+      // Convierte cada Point2f a un Point
+      Point point = Point(point2f.x.toInt(), point2f.y.toInt());
+
+      // Agrega cada Point a una lista individual
+      List<Point> listVecPoint = [point];
+
+      // Agrega la lista de Point a la lista de listas de Point
+      listVecVecPoint.add(listVecPoint);
+    }
+
+    // Convierte la lista de listas de Point a VecVecPoint
+    VecVecPoint vecVecPoint = VecVecPoint.fromList(listVecVecPoint);
+
+    return vecVecPoint;
   }
 
   @override
